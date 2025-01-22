e95db9859996c2672975482c3a3e2ce795eec234
HarperCC101 <j_turgeon@mail.harpercollege.edu>
2025-01-22 18:22:14 +0000

    Added negative test case for FactorialService

--- FactorialLibrary.Tests/FactorialServiceTests.cs
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -1,12 +1,24 @@
-namespace FactorialLibrary.Tests;
-using FactorialLibrary;
-
-public class FactorialServiceTests
+namespace FactorialService.Tests
 {
-  [Fact]
-  public void Test1()
-  {
-    var result = FactorialService.Factorial(0);
-    Assert.Equal(1, result);
-  }
+    using FactorialLibrary;
+    using Xunit;
+
+    public class FactorialServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var result = FactorialService.Factorial(0);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Test_negative()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var result = FactorialService.Factorial(-1);
+            });
+        }
+    }
 }
